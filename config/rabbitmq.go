080692e16912b3c62e79adb7920442d175b8b572
felixyangsen <felixyangsen96@gmail.com>
2021-11-15 16:40:59 +0700

    .vscode

--- config/rabbitmq.go
+++ config/rabbitmq.go
@@ -1,15 +1,17 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/streadway/amqp"
 )
 
 var (
 	rabbitmqCon *amqp.Connection
 	rabbitmqCh  *amqp.Channel
-	// rabbitmqDsn = fmt.Sprintf("amqps://%s:%s@%s:%s/", os.Getenv("RABBITMQ_USER"), os.Getenv("RABBITMQ_PASSWORD"), os.Getenv("RABBITMQ_HOST"), os.Getenv("RABBITMQ_PORT"))
-	// rabbitmqDsn = "amqp://root:krS734TNwvjW@rabbitmq.pmberjaya.com:5672/dextion-alpha"
-	rabbitmqDsn = "amqp://guest:guest@localhost:5672/"
+	rabbitmqDsn = fmt.Sprintf("amqps://%s:%s@%s:%s/%s", os.Getenv("RABBITMQ_USER"), os.Getenv("RABBITMQ_PASSWORD"), os.Getenv("RABBITMQ_HOST"), os.Getenv("RABBITMQ_PORT"), os.Getenv("RABBITMQ_ENV"))
+	// rabbitmqDsn = "amqp://guest:guest@localhost:5672/"
 )
 
 func ConnectRabbitmq() {
